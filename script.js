e3bfb642b2812f6c719d52722f24a0fc6aeeaad8
Debjitpaul19 <pauldebjit27@gmail.com>
2026-04-15 20:22:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const links=document.querySelectorAll("nav a")
+
+links.forEach(link=>{
+
+link.addEventListener("click",()=>{
+
+document.querySelector("nav").classList.remove("active")
+
+})
+
+})
